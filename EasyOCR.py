47d37672446756ba982cf378b400c55390b019c1
JoeCWC <joe02230888@gmail.com>
2024-12-18 23:28:18 +0800

    modify annotation

--- EasyOCR.py
+++ EasyOCR.py
@@ -20,26 +20,26 @@ def find_contours(image):
     #else:
         #print(f"[Info] Start processing {input_image}")
 
-    # Resize image
+    '''Resize image'''
     image = imutils.resize(image, width=500)
     img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Display the original image
+    '''Display the original image'''
     #fig, ax = plt.subplots(2, 2, figsize=(10, 7))
     #ax[0, 0].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     #ax[0, 0].set_title('Original Image')
 
-    # RGB to Gray scale conversion
+    '''RGB to Gray scale conversion'''
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     #ax[0, 1].imshow(gray, cmap='gray')
     #ax[0, 1].set_title('Grayscale Conversion')
 
-    # Noise removal with iterative bilateral filter(removes noise while preserving edges)
+    '''Noise removal with iterative bilateral filter(removes noise while preserving edges)'''
     gray = cv2.bilateralFilter(gray, 11, 17, 17)
     #ax[1, 0].imshow(gray, cmap='gray')
     #ax[1, 0].set_title('Bilateral Filter')
 
-    # Find Edges of the grayscale image
+    '''Find Edges of the grayscale image'''
     edged = cv2.Canny(gray, 170, 200)
     #ax[1, 1].imshow(edged, cmap='gray')
     #ax[1, 1].set_title('Canny Edges')
@@ -47,11 +47,11 @@ def find_contours(image):
     #fig.tight_layout()
     #plt.show()
 
-    # Find contours based on Edges
+    '''Find contours based on Edges'''
     cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:30]  # sort contours based on their area keeping minimum required area as '30' (anything smaller than this will not be considered)
     NumberPlateCnt = None  # we currently have no Number plate contour
-    # loop over our contours to find the best possible approximate contour of number plate
+    '''loop over our contours to find the best possible approximate contour of number plate'''
     count = 0
     for c in cnts:
         peri = cv2.arcLength(c, True)
@@ -67,9 +67,8 @@ def find_contours(image):
             print(f"[Error] Contour is {NumberPlateCnt}: {input_image}")
             return False
         elif NumberPlateCnt is not None:
-            # Drawing the selected contour on the original image
+            '''Drawing the selected contour on the original image'''
             cv2.drawContours(image, [NumberPlateCnt], -1, (0, 255, 0), 3)
-            #show_detection(gray,approx,NumberPlateCnt,ROI,image)
             return show_detection(gray,approx,NumberPlateCnt,ROI,image,input_image)
     except Exception as e:
         print(f"[ERROR] No contours found, {e}")
@@ -79,14 +78,14 @@ def show_detection(gray,approx,NumberPlateCnt,ROI,image,input_image):
     fig, ax = plt.subplots(2, 2, figsize=(10, 7))
     ax[0, 0].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     ax[0, 0].set_title("Detected license plate")
-    # Find bounding box and extract ROI (Region of Interest)
+    '''Find bounding box and extract ROI (Region of Interest)'''
     ax[0, 1].imshow(ROI)
     ax[0, 1].set_title("Extracted license plate")
 
     mask = np.zeros(gray.shape, np.uint8)
     new_image = cv2.drawContours(mask, [NumberPlateCnt], 0, 255, -1)
 
-    # cv2.bitwise_and: Applies the mask to the original image, keeping only the region of interest (the license plate or detected object).
+    '''cv2.bitwise_and: Applies the mask to the original image, keeping only the region of interest (the license plate or detected object)'''
     new_image = cv2.bitwise_and(image, image, mask=mask)
     ax[1, 0].imshow(cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB))
     ax[1, 0].set_title("bitwise")
@@ -101,9 +100,7 @@ def show_detection(gray,approx,NumberPlateCnt,ROI,image,input_image):
         return False
     text = result[0][1]
     font = cv2.FONT_HERSHEY_SIMPLEX
-    res = cv2.putText(image, text=text, org=(approx[1][0][1], approx[0][0][0] + 70), fontFace=font, fontScale=1,
-                      color=(0, 255, 0), thickness=2)
-    # res = cv2.rectangle(image, tuple(approx[0][0]), tuple(approx[2][0]), (0, 255, 0), 3)
+    res = cv2.putText(image, text=text, org=(approx[1][0][1], approx[0][0][0] + 70), fontFace=font, fontScale=1,color=(0, 255, 0), thickness=2)
     ax[1, 1].imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
     ax[1, 1].set_title("Detection: " + text)
     #fig.tight_layout()
@@ -118,14 +115,11 @@ def main(folder_path,image_format):
         if success == False:
             failed_detection.append(image)
             #print(f"[Error] Skipping {image_path} due to processing failure.")
-
         else:
             successful_detection.append(image)
     print("========== Result ==========")
     print(f"Successful: {successful_detection}")
     print(f"Failed: {failed_detection}")
-
-
 
 '''===== Configs ====='''
 #folder_path="material/successful_detection"
