66d22e7e2444e4e06d88004305b636ca596fefd1
JoeCWC <joe02230888@gmail.com>
2024-12-18 23:14:11 +0800

    add error handling

--- EasyOCR.py
+++ EasyOCR.py
@@ -7,21 +7,9 @@ import matplotlib.pyplot as plt
 import easyocr
 import glob
 
-def import_image(input_image):
-    input_file = input_image
-    if os.path.isfile(input_file):
-        print(f"The file '{input_file}' exists.")
-        image = cv2.imread(input_file)
-        return image
-    else:
-        print("[ERROR]", f"The file '{input_file}' does not exist.")
-        exit()
-
-def import_image_folder(folder_path,image_format):
-    image_folder=glob.glob(folder_path+"/"+image_format)
+def import_image_folder(folder_path):
+    image_folder=glob.glob(folder_path)
     return image_folder
-
-
 
 def find_contours(image):
     input_image=image
@@ -63,7 +51,6 @@ def find_contours(image):
     cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:30]  # sort contours based on their area keeping minimum required area as '30' (anything smaller than this will not be considered)
     NumberPlateCnt = None  # we currently have no Number plate contour
-
     # loop over our contours to find the best possible approximate contour of number plate
     count = 0
     for c in cnts:
@@ -74,17 +61,21 @@ def find_contours(image):
             x, y, w, h = cv2.boundingRect(c)
             ROI = img[y:y + h, x:x + w]
             break
-    #print(f"[DEBUG] NumberPlateCnt:\n{NumberPlateCnt}")
+    #print(f"[DEBUG] {input_image} NumberPlateCnt:\n{NumberPlateCnt}")
     try:
-        if NumberPlateCnt is not None:
+        if NumberPlateCnt is None:
+            print(f"[Error] Contour is {NumberPlateCnt} : {input_image}")
+            return False
+        elif NumberPlateCnt is not None:
             # Drawing the selected contour on the original image
             cv2.drawContours(image, [NumberPlateCnt], -1, (0, 255, 0), 3)
-            show_detection(gray,approx,NumberPlateCnt,ROI,image)
+            #show_detection(gray,approx,NumberPlateCnt,ROI,image)
+            return show_detection(gray,approx,NumberPlateCnt,ROI,image,input_image)
     except Exception as e:
-        print(f"[ERROR] No contours found {e}")
+        print(f"[ERROR] No contours found, {e}")
         return False
 
-def show_detection(gray,approx,NumberPlateCnt,ROI,image):
+def show_detection(gray,approx,NumberPlateCnt,ROI,image,input_image):
     fig, ax = plt.subplots(2, 2, figsize=(10, 7))
     ax[0, 0].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     ax[0, 0].set_title("Detected license plate")
@@ -103,44 +94,50 @@ def show_detection(gray,approx,NumberPlateCnt,ROI,image):
     reader = easyocr.Reader(['en'])
     result = reader.readtext(ROI)
 
-
-    print(f"Detection: {result[0][1]}")
+    try:
+        print(f"Detection: {result[0][1]}")
+    except Exception as e:
+        print(f"[Error] {e}: {input_image}")
+        return False
     text = result[0][1]
     font = cv2.FONT_HERSHEY_SIMPLEX
-    res = cv2.putText(image, text=text, org=(approx[1][0][1], approx[0][0][0] + 70), fontFace=font, fontScale=1,color=(0, 255, 0), thickness=2)
+    res = cv2.putText(image, text=text, org=(approx[1][0][1], approx[0][0][0] + 70), fontFace=font, fontScale=1,
+                      color=(0, 255, 0), thickness=2)
     # res = cv2.rectangle(image, tuple(approx[0][0]), tuple(approx[2][0]), (0, 255, 0), 3)
     ax[1, 1].imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
     ax[1, 1].set_title("Detection: " + text)
-    #fig.tight_layout()
-    #plt.show()
+    fig.tight_layout()
+    plt.show()
+
 
 
 def main(folder_path,image_format):
-    image_path = import_image_folder(folder_path,image_format)
+    image_path = import_image_folder(folder_path+image_format)
     successful_detection=[]
     failed_detection=[]
     for image in image_path:
         success = find_contours(image)
         if success == False:
             failed_detection.append(image)
-            print(f"Skipping {image_path} due to processing failure.")
-            continue
+            #print(f"[Error] Skipping {image_path} due to processing failure.")
+
         else:
             successful_detection.append(image)
+    print("========== Result ==========")
     print(f"Successful: {successful_detection}")
     print(f"Failed: {failed_detection}")
 
-folder_path="material/successful_detection"
-image_format="*.jpg"
 
-main(folder_path,image_format)
 
-#import_image_folder(folder_path,image_format)
-#input_image="/Users/Joe/Documents/Uni_Taipei/Image_Process/License_Plate_Detection/POC/material/successful_detection/plate01.jpg"
-#input_image="/Users/Joe/Documents/Uni_Taipei/Image_Process/License_Plate_Detection/POC/material/failed_detection/plate05.jpg"
-#image=import_image(input_image)
-#gray,approx,NumberPlateCnt,ROI,image=find_contours(image)
-#show_detection(gray,approx,NumberPlateCnt,ROI,image)
+#===== Configs =====
+#folder_path="material/successful_detection"
+folder_path="material/failed_detection"
+image_format="/*"
+
+#===== main =====
+if __name__ == "__main__":
+    main(folder_path,image_format)
+
 
 
 
